294d2dbdcb05edd28b88d98aabf0cf525662c4c6
Avijeet pandey <avijeetpandey25@gmail.com>
2024-03-23 10:31:31 +0530

    Add: init code

---
+++ BarCodeScanner-SwiftUI/BarCodeScanner_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScanner_SwiftUIApp.swift
+//  BarCodeScanner-SwiftUI
+//
+//  Created by Avijeet on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScanner_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarCodeView()
+        }
+    }
+}
---
+++ BarCodeScanner-SwiftUI/BarCodeView.swift
@@ -0,0 +1,24 @@
+//
+//  BarCodeView.swift
+//  BarCodeScanner-SwiftUI
+//
+//  Created by Avijeet on 23/03/24.
+//
+
+import SwiftUI
+
+struct BarCodeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    BarCodeView()
+}
